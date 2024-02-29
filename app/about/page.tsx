ce2f18fbbd8d15f1048b00dd1f8fff172aa4d04c
asdcf35 <prann1234556@gmail.com>
2024-02-28 23:07:20 -0500

    asdfasdfasdf

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,24 @@
-import { title } from "@/components/primitives";
+import { title, subtitle } from "@/components/primitives";
+import {Divider} from "@nextui-org/react";
 
+import "./class.css"
 export default function AboutPage() {
-	return (
-		<div>
-			<h1 className={title()}>About</h1>
-		</div>
-	);
+  return (
+    <>
+      <div className="flx">
+			<img
+          src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+          className="max-w-sm rounded-lg shadow-2xl"/>
+        <div className="flx-col">
+          <h1 className={title()}>Kyle Snyder</h1>
+		  <br></br>
+          <p className={subtitle({ class: "mt-2	" })}>Our Candidate</p>
+        </div>
+      </div>
+	  <br></br>
+ 	  <div>
+	   <p className="df">Backstory</p>
+	  </div>
+    </>
+  );
 }
