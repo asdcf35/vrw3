440f3829352bcdaf85d5cde9f8b1920e8ccc3631
asdcf35 <prann1234556@gmail.com>
2024-03-01 03:51:40 +0000

    Feb 29, 2024, 10:51 PM

--- app/platform/page.tsx
+++ app/platform/page.tsx
@@ -7,7 +7,7 @@ export default function App() {
     <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
     <Card className="col-span-12 sm:col-span-4 h-[300px]" isPressable onPress={() => console.log("item pressed")}>
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-        <p className="text-tiny text-white/60 uppercase font-bold">Platform #2</p>
+        <p className="text-tiny text-white/60 uppercase font-bold">Plank #1</p>
         <h4 className="text-white font-medium text-large">Increasing Wages - Step by Step</h4>
       </CardHeader>
       <Image
@@ -20,13 +20,13 @@ export default function App() {
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
       <CardHeader className="absolute z-10 top-1 flex-col !items-start">
         <p className="text-tiny text-white/60 uppercase font-bold">Plank #2</p>
-        <h4 className="text-white font-medium text-large">Reforming Educatio</h4>
+        <h4 className="text-white font-medium text-large">Reforming Education</h4>
       </CardHeader>
       <Image
         removeWrapper
         alt="Card background"
         className="z-0 w-full h-full object-cover"
-        src=""
+        src="https://habitatbroward.org/wp-content/uploads/2020/01/10-Benefits-Showing-Why-Education-Is-Important-to-Our-Society.jpg"
       />
     </Card>
     <Card className="col-span-12 sm:col-span-4 h-[300px]">
