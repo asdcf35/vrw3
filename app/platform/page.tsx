307a817eb59852996426e28eadf6b235a08732c3
asdcf35 <prann1234556@gmail.com>
2024-03-01 03:55:08 +0000

    Feb 29, 2024, 10:55 PM

--- app/platform/page.tsx
+++ app/platform/page.tsx
@@ -55,11 +55,8 @@ export default function App() {
       <CardFooter className="absolute bg-white/30 bottom-0 border-t-1 border-zinc-100/50 z-10 justify-between">
         <div>
           <p className="text-black text-tiny">Plank #4</p>
-          <p className="text-black text-tiny">Get notified.</p>
+          <p className="text-black text-tiny">A novel way to solve immigration.</p>
         </div>
-        <Button className="text-tiny" color="primary" radius="full" size="sm">
-          Notify Me
-        </Button>
       </CardFooter>
     </Card>
     <Card isFooterBlurred className="w-full h-[300px] col-span-12 sm:col-span-7">
