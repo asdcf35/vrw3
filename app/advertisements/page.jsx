cc278095333c0c5ba2ececaa21374e693c3cffdf
asdcf35 <prann1234556@gmail.com>
2024-03-01 04:44:08 +0000

    Feb 29, 2024, 11:44 PM

--- app/advertisements/page.jsx
+++ app/advertisements/page.jsx
@@ -338,7 +338,7 @@ export default function App() {
           >
             <title>Oregon</title>
           </path>
-          <Tooltip content="I am a tooltip">
+          <Link href="@app/ui/">
           <path
             className="pa"
             d="m 826.3,189.4 -1.9,.3 -3,-2.2 -3,-5.8 h -2 l -.4,-1.5 -1.7,-1.1 -70.5,13.9 -.8,-6 -4.2,3.4 -.9,.1 -2.7,3 -3.3,1.7 4.9,29.9 3.2,19.7 17.4,-2.9 60.5,-11.8 1.2,-2.1 1.5,-1.1 1.6,-.3 1.6,.6 1.4,-1.7 1.6,-.6 1.8,-3 1.6,-2.3 3.3,-2.6 -4.2,-3.2 -2.1,-1.1 -1,-2.8 -2.7,-.9 -.5,-3.6 1,-1 .7,-2 -1.5,-1.8 3,-5.4 -.1,-2.2 1.8,-2.5 z"
@@ -347,7 +347,7 @@ export default function App() {
          
             <title>Pennsylvania</title>
           </path>
-          </Tooltip>
+          </Link>
 
           <path
             className="ri"
