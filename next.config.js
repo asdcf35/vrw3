1bada8c2dd123dbd7c4be00d1bb99f73113ea1e9
asdcf35 <prann1234556@gmail.com>
2024-03-01 18:56:50 +0000

    jjkjk

--- next.config.js
+++ next.config.js
@@ -1,11 +1,2 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
-  
-}
+module.exports = {}
